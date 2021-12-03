4df97f4c55e22fee20b6a0e4baa09c8d4ff24514
davidkc <dakchoy@gmail.com>
2021-12-03 14:17:55 +0400

    first commit

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ footer_tooltip.addEventListener("click",()=>{
     window.open("https://en.wikipedia.org/wiki/Emma_Raducanu")
 })
 
-// //make background blur when hover on main image
-// document.querySelector("#image").addEventListener("mouseenter",()=>{
-//     document.querySelector("#main").classList.toggle("change")
-// })
-// document.querySelector("#image").addEventListener("mouseleave",()=>{
-//     document.querySelector("#main").classList.toggle("change")
-// })
-
 //prevent right-click/longpress on Safari
 document.querySelector("#image").addEventListener("contextmenu",e=>{
     e.preventDefault()
